b9259e400344f128c569b4897e59a07048d64e6a
vyom210803 <vyom2108@gmail.com>
2022-06-09 02:44:22 -0400

    Written variables provide brief information

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,11 +2,25 @@
 # My fourth script - sysinfo.sh
 # This script displays different strings
 
-echo 'Host Name:'
-hostname
-echo 'Domain Name: ubuntu'
-echo 'Operating System name and version: ' 
-echo "LAN IP ADDRESS:"
-ip a s enp0s3 | awk '/inet /{print$2}'
-echo "Root File Status:"
-df -h
+#############
+##Variable###
+#############
+
+hn=$(hostname --fqdn)
+sv=$(uname -a)
+ia=$(ip a s enp0s3 | awk '/inet /{print$2}')
+fs=$(df -h | awk '/1.5M /{print $4}')
+
+#############
+##Main Body##
+#############
+echo '                                   '
+echo 'Report for Virtual Machine'
+#In next steps we will consider some basic variables
+
+echo '=========================='
+echo "FQDN: $hn"
+echo "Full details of Operating System: $sv" 
+echo "LAN IP ADDRESS: $ia"
+echo "Root File Status: $fs"
+echo '=========================='
